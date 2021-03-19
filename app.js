80929c4f0a2397d237dcfdab0f5a985d23482669
fomich999 <fomas.ukr.net@gmail.com>
2021-03-19 12:18:20 +0200

    new app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+// https://www.youtube.com/watch?v=Bluxbh9CaQ0
+// Исходники в телеграм канале:
+//https://teleg.one/js_by_vladilen​
+
+// Number
+const num = 42 // integer
+const float = 42.42 // float
+const pow = 10e3
+console.log(pow)
