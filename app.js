2c5714fa2df4d8e4c014c0ec8971fc16e9c15b43
fomich999 <fomas.ukr.net@gmail.com>
2021-04-16 16:15:52 +0300

    New changelist3

---
+++ app.js
@@ -0,0 +1,21 @@
+// Функции
+
+// FUNCTION EXPRESSION
+ ////const greet2 = function(name) {
+ //}
+const greet2 = function greet2(name) {
+    console.log('Привет 2- ', name)
+}
+
+greet( 'Лена')
+greet2( 'Леша')
+
+// FUNCTION DECLARATION
+function greet(name) {
+    console.log('Привет - ', name)
+}
+
+// console.log(typeof greet)
+// console.dir(greet)
+
+// Анонимные функции
