9376f96a991ae450ec4797cf1734943013a1de55
fomich999 <fomas.ukr.net@gmail.com>
2021-04-16 13:00:54 +0300

    New changelist2

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-const name = 'Alex'
-console.log(name)
-const age = 26
+const name = 'Александр'
+const age = 30
+
+const output = 'Привет, меня зовут ' + name + ' и мой возраст: ' + age + 'лет'
+console.log(output)
