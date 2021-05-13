510d1f0448a743e8b5056f6af111f4d0b64bd671
fomich999 <fomas.ukr.net@gmail.com>
2021-05-13 16:28:06 +0300

    New changelist3

--- app.js
+++ app.js
@@ -30,11 +30,11 @@
 
 // 3 Стрелочные функции
 function greet(name) {
-    console.log('Привет - ', name)
+    //console.log('Привет - ', name)
 }
 
 const arrow = (name, age) => {
-    console.log('Привет - ', name, age)
+    //console.log('Привет - ', name, age)
 }
 //arrow('Alexandr')
 
@@ -48,13 +48,29 @@ const pow2 = num => num ** 2
 // 4 Параметры по умолчанию
 const sum = (a = 40, b= a * 2) => a +b
 
-console.log(sum(41, 4))
-console.log(sum())
+// console.log(sum(41, 4))
+// console.log(sum())
 
 function sumAll(...all){
-    //const result = 0
-    //for ()
-    console.log(all)
+    let result = 0
+    for (let num of all) {
+        result += num
+    }
+    //console.log(all)
+    return result
 }
 
-sumAll(1, 2, 3, 4, 5, 6, 7)
+const res = sumAll(1, 2, 3, 4, 5, 6, 7)
+//console.log(res)
+
+// 5 Замыкания
+function createMember(name) {
+    return function (lastName) {
+        console.log(name + lastName)
+    }
+}
+
+const logWithLastName = createMember('Alexandr')
+console.log(LogWithLastName)
+// console.log(LogWithLastName('Fomichev'))
+// console.log(LogWithLastName('Minin'))
