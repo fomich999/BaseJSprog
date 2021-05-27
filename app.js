606182777bb5db3c05fd77cd13b6ac3d63f3c3fe
nvox <607665>
2021-05-27 13:03:00 +0300

    27 may

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
 // Массивы
+const cars = ['Мазда', 'Форд', 'БМВ', 'Мерседес']
+const fib = [1, 1, 2, 3, 5, 8, 13]
 
+// Function
+function addItemToEnd() {
+
+}
+// Method
+// cars.push ('Рено')
+// cars.unshift('Волга')
+
+// const firstItem = cars.shift()
+//const lastCar = cars.pop()
+//console.log(lastCar)
+// console.log(firstItem)
+// console.log(cars)
+
+//console.log(cars.reverse())
+//console.log(cars)
+
+// Задача 1
+const text = 'Привет, мы изучаем JavaScript'
+let  reverseText = text.split(',')
+console.log(reverseText)
