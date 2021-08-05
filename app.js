42da37e3ec5af0cc4cb933dfda42fb424057afae
fomich999 <fomas.ukr.net@gmail.com>
2021-08-05 13:48:35 +0300

    05_08_21_//Context

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const person = {
    age: 37,
    isProgrammer: true,
    languages: ['ru','en', 'de'],
-   'complex key': 'Complex Value',
-['key_' + (1+3)]: 'Computed Key', // key_4
+   // 'complex key': 'Complex Value',
+   // ['key_' + (1+3)]: 'Computed Key', // key_4
    greet() {
       console.log('greet from person')
-
+   },
+   info() {
+      console.log('this:', this)
+     console.info('Информация про человека по имени:', this.name)
    }
 }
+
 // console.log(person.name)
 // console.log(person['age'])
 //
@@ -31,5 +35,50 @@ const person = {
 // const  age = person.age
 // const languages =person.languages
 
-const {name, age, languages} = person
-console.log(name,age,languages)
+//const {name, age: personAge = 15, languages} = person
+//console.log(name,personAge,languages)
+
+//console.log(person)
+
+// for (let key in person) {
+//    if (person.hasOwnProperty(key)) {
+//    console.log('key:', key)
+//    console.log('value:', person[key])
+//
+//   }
+// }
+
+// получаем массив
+// const keys = Object.keys(person)
+
+// используем метод массива forEach и делаетм итерацию по массиву, принимая в колбек функцию каждый итерируемый элемент(key)
+// keys.forEach((key) => {
+//    console.log('key:', key)
+//    console.log('value:', person[key])
+// })
+
+// то же самое только для полученного массива сразу же вызываем forEach
+// Object.keys(person).forEach((key) => {
+//    console.log('key:', key)
+//    console.log('value:', person[key])
+// })
+
+// Context
+// person.info()
+
+const logger = {
+   keys() {
+      console.log('Object keys:', Object.keys(this))
+   },
+
+   keysAndValues() {
+      // "key": value
+      Object.keys(this).forEach(key => {
+         console.log('"${key}": ${this[key]}')
+      })
+   }
+}
+// const bound = logger.keys.bind(person)
+// bound()
+//logger.keys.call(person)
+logger.keysAndValues.call(logger)
