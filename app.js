99177021af38130870918e79a3406020015ee47d
nvox <607665>
2021-09-14 16:32:42 +0300

    8 lesson added end

--- app.js
+++ app.js
@@ -6,29 +6,34 @@ const heading = document.getElementById('hello')
 // console.log(heading2[0]);
 // const heading2 = document.getElementsByClassName('h2-class')
 // console.log(heading2);
-
 // const heading2 = document.querySelector('#sub-hello') // всегда 1 элемент
 const heading2 = document.querySelector('h2') //
 
-console.dir(heading2);
-
 // const heading3 = heading2.nextElementSibling
 const h2list =document.querySelectorAll('h2')
-console.log(h2list);
 const heading3 =h2list[h2list.length - 1]
-console.log(heading3)
+
 
 setTimeout( () => {
    addStylesTo(heading, 'JavaScript', 'red', '3rem')
+}, 1000)
+
+const link = heading3.querySelector('a')
+link.addEventListener( 'click', (event) => {
+    event.preventDefault()
+    console.log('Click on link', event.target.getAttribute('href'))
+    const url = event.target.getAttribute('href')
+
+    setTimeout( () => window.location = url, 2000)
+})
+
+setTimeout( () => {
+    addStylesTo(heading3.children[0], 'Практикуйся', 'blue')
 }, 1500)
 
 setTimeout( () => {
-    addStylesTo(heading3, 'Практикуйся', 'blue')
-}, 3000)
-
-setTimeout( () => {
     addStylesTo(heading2, 'И всё получится!', 'yellow', '2rem' )
-}, 4500)
+}, 2000)
 
 function addStylesTo(node, text, color = 'red', fontSize) {
     node.textContent = text
@@ -36,6 +41,9 @@ function addStylesTo(node, text, color = 'red', fontSize) {
     node.style.textAlign = 'center'
     node.style.backgroundColor = 'black'
     node.style.padding = '2rem'
+    node.style.display = 'block'
+    node.style.width = '100%'
+
     // falsy: '', undefined, null, 0, false
     if (fontSize) {
         node.style.fontSize = fontSize
@@ -44,6 +52,20 @@ function addStylesTo(node, text, color = 'red', fontSize) {
 // https://developer.mozilla.org/ru/docs/Web/Events
 heading.onclick = () => {
     if (heading.style.color === 'red') {
-
+        heading.style.color = '#000'
+        heading.style.backgroundColor = '#fff'
+    } else {
+      heading.style.color = 'red'
+      heading.style.backgroundColor = '#000'
     }
 }
+
+heading2.addEventListener('dblclick', () => {
+    if (heading2.style.color === 'yellow') {
+        heading2.style.color = '#000'
+        heading2.style.backgroundColor = '#fff'
+    } else {
+        heading2.style.color = 'yellow'
+        heading2.style.backgroundColor = '#000'
+    }
+})
