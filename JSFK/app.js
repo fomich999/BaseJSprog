0e8cff46fbb6047479b4135a34fb90b062a63363
nvox <607665>
2021-09-16 16:41:24 +0300

    first pages of JSFK

--- JSFK/app.js
+++ JSFK/app.js
@@ -1,7 +1,27 @@
-// Рисуем столько котиков, сколько захотим!
-var drawCats = function (howManyTimes) {
-    for (var i = 0; i < howManyTimes; i++) {
-        console.log(i + " =^.^=");
-    }
-};
-drawCats(10); // Вместо 10 тут может быть другое число
+// // Рисуем столько котиков, сколько захотим!
+// var drawCats = function (howManyTimes) {
+//     for (var i = 0; i < howManyTimes; i++) {
+//         console.log(i + " =^.^=");
+//     }
+// };
+// drawCats(10); // Вместо 10 тут может быть другое число
+
+// 1234 + 57 * 3 - 31 / 4 // 57*3 = 171
+// 1234 + 171 - 31 / 4    // 31/4 = 7.75
+// 1234 + 171 - 7.75      // 1234 + 171 = 1405
+// 1405 - 7.75
+// 1397.25
+
+// var score = 10;
+// console.log(score += 7, score -= 3)
+// var balloons = 100;
+// console.log(balloons *= 2, balloons /= 4)
+//
+// var javascript = "java" + "script";
+//
+// console.log(javascript.length)
+// console.log("script".length);
+//
+// console.log("Вот длинная строка".slice(0, 3));
+// var str = "Вот длинная строка"
+// console.log(str[0]+[5]);
